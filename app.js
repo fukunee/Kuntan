2cef72d4a46c259dab8ceab3e301970bf9687a2f
fukunee@傅堃 <fukun.ee@foxmail.com>
2019-12-11 20:00:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,10 +18,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // router setup
-var router = express.Router();
+const router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
