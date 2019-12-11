f47e66aeb9e8e1d424a22c04e2a3bb16b135feb2
fukunee <fukun.ee@foxmail.com>
2019-12-11 17:57:40 +0800

    Arch finished
    B/S Arch
    WS drive

---
+++ models/redisClient.js
@@ -0,0 +1,40 @@
+var redisClient = require("redis");
+
+class RedisClient {
+    static getInstance() {
+        if (!RedisClient.instance) {
+            RedisClient.instance = new RedisClient();
+        }
+        return RedisClient.instance;
+    }
+
+    constructor() {
+        this.client = redisClient.createClient("6379", "47.244.224.86");
+        this.client.on("error", function (err) {
+            console.log("Error " + err);
+        });
+    }
+}
+
+RedisClient.prototype.set = function (id, key, val) {
+    this.client.select(id, function () {
+        this.client.set("key", "val", redisClient.print);
+    })
+}
+RedisClient.prototype.get = function (id, key, val, next) {
+    this.client.select(id, function () {
+        this.client.get("string key", next);
+    })
+}
+
+//!todo redisClient
+// client.hset("hash key", "hashtest 1", "some value", redisClient.print);
+// client.hset(["hash key", "hashtest 2", "some other value"], redisClient.print);
+// client.hkeys("hash key", function (err, replies) {
+//     console.log(replies.length + " replies:");
+//     replies.forEach(function (reply, i) {
+//         console.log("    " + i + ": " + reply);
+//     });
+//     client.quit();
+// });
+module.exports = RedisClient;
---
+++ ws.js
@@ -0,0 +1,20 @@
+function ws(server) {
+    this.io = require('socket.io')(server);
+    io.on('connection', ws.on);
+}
+
+// websocket controller
+ws.on = function (socket) {
+    console.log('a user connected');
+
+    // @Disconnect
+    socket.on("disconnect", function () {
+        console.log("a user go out");
+    });
+    socket.emit("msg");
+    socket.on("msg", function (obj) {
+        console.log('the websokcet message is' + JSON.stringify(obj));
+    })
+}
+
+module.exports = ws;
